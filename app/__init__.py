b1a6614b470d7bcc52c1fbe5382c08a7b07e4cbe
shubham.mankodiya <shubhamm.spaceo@gmail.com>
2024-04-04 20:12:20 +0530

    Check for GPU

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+import tensorflow as tf
 
 app = Flask(__name__)
+
+# Check if GPU is available
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    # Restrict TensorFlow to only use the first GPU
+    try:
+        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+        # Visible devices must be set before GPUs have been initialized
+        print(e)
+else:
+    print("No GPU available, falling back to CPU.")
 
 from app import routes
 
